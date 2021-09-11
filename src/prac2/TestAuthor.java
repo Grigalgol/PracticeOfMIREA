8dff62c8b1d24f1f92b1f136d1783894441eeca6
Grigoriy <grisha101202@yandex,ru>
2021-09-11 17:48:12 +0300

    ended 2 practice

---
+++ src/prac2/TestAuthor.java
@@ -0,0 +1,8 @@
+package prac2;
+
+public class TestAuthor {
+    public static void main(String[] args) {
+        Author author = new Author("Grisha", "grisha101202@yandex.ru", 'm');
+        System.out.println(author.toString());
+    }
+}
